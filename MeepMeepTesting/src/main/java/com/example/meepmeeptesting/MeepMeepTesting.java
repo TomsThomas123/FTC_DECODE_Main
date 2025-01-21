e5d376d5debf6e332fe387c3ff5bce1140ad1734
Masterpro15 <evanckurian@gmail.com>
2025-01-21 16:44:35 -0600

    expermenting auto with meep mep -evan

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,28 +6,23 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(500);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, -61, Math.toRadians(90)))
-                //.lineToY(-34)
-                //.setTangent(0)
-                //.strafeTo(new Vector2d(16, -34))
-                //.splineToSplineHeading(new Pose2d(36, -10, Math.toRadians(270)), Math.PI / 2)
-                //.strafeTo(new Vector2d(46, -10))
+
                 .strafeTo(new Vector2d(0, -30))
                 .waitSeconds(1)
 
                 .setReversed(true)
                 .splineToSplineHeading(new Pose2d(new Vector2d(26,-43), Math.toRadians(-90)), 0)
-                //.afterTime(0, shoulder.autonDown())
+
 
                 .splineTo(new Vector2d(45, -13), 0)
 
@@ -38,77 +33,24 @@ public class MeepMeepTesting {
                 .strafeTo(new Vector2d(55,-13))
                 .strafeTo(new Vector2d(55,-59))
                 .waitSeconds(0.5)
-                //undo ^ if something goes wrong.
+
                 .setReversed(true)
                 .splineToSplineHeading(new Pose2d(new Vector2d(0,-35), Math.toRadians(-270)), Math.toRadians(180))
                 .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(55,-57), Math.toRadians(270)), Math.toRadians(270))
+                .splineToSplineHeading(new Pose2d(new Vector2d(38, -59.5), Math.toRadians(270)), Math.toRadians(270))
                         .waitSeconds(0.5)
                         .setReversed(true)
                 .splineToSplineHeading(new Pose2d(new Vector2d(0,-35), Math.toRadians(-270)), Math.toRadians(180))
                 .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(55,-57), Math.toRadians(270)), Math.toRadians(270))
+                .splineToSplineHeading(new Pose2d(new Vector2d(38, -59.5), Math.toRadians(270)), Math.toRadians(270))
                 .waitSeconds(0.5)
                 .setReversed(true)
                 .splineToSplineHeading(new Pose2d(new Vector2d(0,-35), Math.toRadians(-270)), Math.toRadians(180))
-                /*.strafeTo(new Vector2d(46,-60))
-
-                //.afterTime(0, claw.autonCloseClaw())
-                .waitSeconds(0.3)
-                //.afterTime(0, shoulder.autonHC())
-                //grab sample, routing towards chamber.
-                //raise arm to clip
-                //.afterTime(1.4, viper.autonHangSpecimen())
                 .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(7, -30), Math.toRadians(90)), Math.toRadians(90))
-
-
-                //clip, routing to push final sample and grab specimen
-
-                //.afterTime(0, claw.autonOpenClaw())
-                //.afterTime(0.1, viper.autonSlightOut())
+                .splineToSplineHeading(new Pose2d(new Vector2d(55,-57), Math.toRadians(-270)), Math.toRadians(180))
 
 
 
-                //.afterTime(1, shoulder.autonDown())
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(35, -59.5), Math.toRadians(-80)), Math.toRadians(0))
-
-                //.afterTime(0, claw.autonCloseClaw())
-                .waitSeconds(0.3)
-                //.afterTime(0, shoulder.autonHC())
-                //grab sample, routing towards chamber.
-                //raise arm to clip
-                //.afterTime(1.5, viper.autonHangSpecimen())
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(4, -30), Math.toRadians(90)), Math.toRadians(90))
-
-                //.afterTime(0, claw.autonOpenClaw())
-                //.afterTime(0, viper.autonSlightOut())
-
-
-                //.afterTime(1, shoulder.autonDown())
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(35, -59.5), Math.toRadians(-80)), Math.toRadians(0))
-
-                //.afterTime(0, claw.autonCloseClaw())
-                .waitSeconds(0.3)
-                //.afterTime(0, shoulder.autonHC())
-                //grab sample, routing towards chamber.
-                //raise arm to clip
-                //.afterTime(1.5, viper.autonHangSpecimen())
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(6, -30), Math.toRadians(90)), Math.toRadians(90))
-
-                //.afterTime(0, claw.autonOpenClaw())
-                //.afterTime(0, viper.autonSlightOut())
-                .waitSeconds(0.5)
-
-                .setReversed(true)
-                .splineTo(new Vector2d(50,-60), Math.toRadians(-90))
-                //.afterTime(0, shoulder.autonDown())
-
-                 */
                 .build());
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Arm {
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1/360.0; // we want ticks per degree, not per rotation
+    final int ARM_COLLAPSED_INTO_ROBOT  = (int)(0*ARM_TICKS_PER_DEGREE);
+    final int ARM_SCORE_SPECIMEN        = (int)(60 * ARM_TICKS_PER_DEGREE);
+    final int ARM_SCORE_SPECIMEN2        = (int)(25 * ARM_TICKS_PER_DEGREE);
+    private DcMotorEx armMotor;
+    private OpMode myOpMode;
+    public Arm(OpMode opmode) {
+        myOpMode = opmode;
+    }
+    int armposition;
+    public void init(){
+        armMotor = myOpMode.hardwareMap.get(DcMotorEx.class, "arm_motor");
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public class ArmDown implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            // desired position, actions do not have parameters (you will have to create a new action for each position you need to go to in auto)
+            armMotor.setTargetPosition(ARM_COLLAPSED_INTO_ROBOT);
+            armMotor.setPower(0.7);// (adjust speed for whatever is necessary)
+            //(you cannot stop motion within this action, you would have to do it in another)
+
+
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            return false;
+        }
+    }
+    public Action armDown() {
+        return new ArmDown();
+    }
+    public class ArmStop implements Action{
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+
+            armMotor.setPower(0);
+            return false;
+        }
+    }
+    public Action armStop(){
+        return new ArmStop();
+    }
+
+}
