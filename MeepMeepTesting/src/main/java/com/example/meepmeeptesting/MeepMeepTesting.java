cedf423c8cb06a35b0f1472c15386e9d72d1fa03
Masterpro15 <evanckurian@gmail.com>
2025-01-21 10:10:51 -0600

    expermenting auto with meep mep -evan

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,30 +1,104 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(500);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, -61, Math.toRadians(90)))
+                //.lineToY(-34)
+                //.setTangent(0)
+                //.strafeTo(new Vector2d(16, -34))
+                //.splineToSplineHeading(new Pose2d(36, -10, Math.toRadians(270)), Math.PI / 2)
+                //.strafeTo(new Vector2d(46, -10))
+                .strafeTo(new Vector2d(0, -30))
+
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(26,-43), Math.toRadians(-90)), 0)
+                //.afterTime(0, shoulder.autonDown())
+
+                .splineTo(new Vector2d(45, -13), 0)
+
+                .strafeTo(new Vector2d(45,-53))
+                //one in observation zone
+                .strafeTo(new Vector2d(45,-13))
+
+                .strafeTo(new Vector2d(55,-13))
+                .strafeTo(new Vector2d(55,-59))
+                .waitSeconds(0.5)
+                //undo ^ if something goes wrong.
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(0,-35), Math.toRadians(-270)), 0)
+                /*.strafeTo(new Vector2d(46,-60))
+
+                //.afterTime(0, claw.autonCloseClaw())
+                .waitSeconds(0.3)
+                //.afterTime(0, shoulder.autonHC())
+                //grab sample, routing towards chamber.
+                //raise arm to clip
+                //.afterTime(1.4, viper.autonHangSpecimen())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(7, -30), Math.toRadians(90)), Math.toRadians(90))
+
+
+                //clip, routing to push final sample and grab specimen
+
+                //.afterTime(0, claw.autonOpenClaw())
+                //.afterTime(0.1, viper.autonSlightOut())
+
+
+
+                //.afterTime(1, shoulder.autonDown())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(35, -59.5), Math.toRadians(-80)), Math.toRadians(0))
+
+                //.afterTime(0, claw.autonCloseClaw())
+                .waitSeconds(0.3)
+                //.afterTime(0, shoulder.autonHC())
+                //grab sample, routing towards chamber.
+                //raise arm to clip
+                //.afterTime(1.5, viper.autonHangSpecimen())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(4, -30), Math.toRadians(90)), Math.toRadians(90))
+
+                //.afterTime(0, claw.autonOpenClaw())
+                //.afterTime(0, viper.autonSlightOut())
+
+
+                //.afterTime(1, shoulder.autonDown())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(35, -59.5), Math.toRadians(-80)), Math.toRadians(0))
+
+                //.afterTime(0, claw.autonCloseClaw())
+                .waitSeconds(0.3)
+                //.afterTime(0, shoulder.autonHC())
+                //grab sample, routing towards chamber.
+                //raise arm to clip
+                //.afterTime(1.5, viper.autonHangSpecimen())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(new Vector2d(6, -30), Math.toRadians(90)), Math.toRadians(90))
+
+                //.afterTime(0, claw.autonOpenClaw())
+                //.afterTime(0, viper.autonSlightOut())
+                .waitSeconds(0.5)
+
+                .setReversed(true)
+                .splineTo(new Vector2d(50,-60), Math.toRadians(-90))
+                //.afterTime(0, shoulder.autonDown())
+
+                 */
                 .build());
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
