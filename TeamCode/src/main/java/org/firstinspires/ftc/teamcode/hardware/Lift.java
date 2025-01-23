2827a0f91da239b1e7ad2039e4ce7fc81a0d0e9a
Masterpro15 <evanckurian@gmail.com>
2025-01-22 21:41:03 -0600

    adding lift hardware class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Lift.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Lift {
+    final double LIFT_TICKS_PER_MM = (111132.0 / 289.0) / 120.0;
+
+    final double LIFT_COLLAPSED = 0 * LIFT_TICKS_PER_MM;
+    final double LIFT_COLLECT =  100 * LIFT_TICKS_PER_MM;
+    final double LIFT_SCORING_IN_LOW_BASKET = 0 * LIFT_TICKS_PER_MM;
+    final double LIFT_SCORING_IN_HIGH_BASKET = 600 * LIFT_TICKS_PER_MM;
+    final double TINY = 600 * LIFT_TICKS_PER_MM;
+
+    double liftPosition = LIFT_COLLAPSED;
+    private DcMotorEx liftMotor;
+    private OpMode myOpMode;
+    public Lift(OpMode opmode) {
+        myOpMode = opmode;
+    }
+    int armposition;
+    public void init(){
+        liftMotor = myOpMode.hardwareMap.get(DcMotorEx.class, "liftMotor");
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public class LiftTiny implements Action {
+        @Override
+
+        public boolean run(@NonNull TelemetryPacket packet) {
+            // desired position, actions do not have parameters (you will have to create a new action for each position you need to go to in auto)
+            liftMotor.setTargetPosition((int) TINY);
+            liftMotor.setPower(0.6);// (adjust speed for whatever is necessary)
+            //(you cannot stop motion within this action, you would have to do it in another)
+            liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            return false;
+        }
+    }
+    public Action liftTiny() {
+        return new LiftTiny();
+    }
+    public class LiftDown implements Action {
+        @Override
+
+        public boolean run(@NonNull TelemetryPacket packet) {
+            // desired position, actions do not have parameters (you will have to create a new action for each position you need to go to in auto)
+            liftMotor.setTargetPosition((int) LIFT_COLLAPSED);
+            liftMotor.setPower(0.6);// (adjust speed for whatever is necessary)
+            //(you cannot stop motion within this action, you would have to do it in another)
+            liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            return false;
+        }
+    }
+    public Action liftDown() {
+        return new LiftDown();
+    }
+}
